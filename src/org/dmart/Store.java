914e29d7b713c7106a6de67d4392960827f2ea0d
githupavi <pavisara0610@gmail.com>
2022-10-03 20:19:54 +0530

    pavifirst

---
+++ src/org/dmart/Store.java
@@ -0,0 +1,16 @@
+package org.dmart;
+
+public class Store {
+	private void materialId() {
+		System.out.println("material id is 456");
+	}
+	private void materialName() {
+		System.out.println("material name is washing powder");
+	}
+	public static void main(String[] args) {
+		Store rin=new Store();
+		rin.materialId();
+		rin.materialName();
+	}
+}
+	
